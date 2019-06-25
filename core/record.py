66fcead5a88ead7e6e7928e66c03e1e966f336b7
Matthias Rombach <matthias.rombach@axoom.com>
2019-06-25 10:40:31 +0200

    Changes record time estimation (ffmpeg)

--- core/record.py
+++ core/record.py
@@ -13,11 +13,12 @@ ffmpeg_debug = True if os.getenv('FFMPEG_DEBUG') else False
 
 def record(file_name, track_name, stop_recording_handler):
     log.info('Recording {} to {}'.format(track_name, file_name))
+    record_process = None
     try:
-        FNULL = open(os.devnull, 'w')
+        fnull = open(os.devnull, 'w')
         record_process = subprocess.Popen(
             ['ffmpeg', '-f', 'pulse', '-i', 'default', '-b:a', '320k', '-ar', '48000', '-f', 'mp3', file_name],
-            stdout=FNULL, stderr=subprocess.STDOUT)
+            stdout=fnull, stderr=subprocess.STDOUT)
         log.debug('started recording')
         timeout = time.time() + 13
         while time.time() < timeout:
@@ -37,10 +38,15 @@ def record(file_name, track_name, stop_recording_handler):
     log.info('Finished recording of {}'.format(track_name))
 
 
-def get_recorded_length(file_name):
-    ffmpeg_get_mediafile_length = ['sh', '-c', 'ffmpeg -i "$1" 2>&1 | grep Duration', '_', file_name]
-    output = subprocess.Popen(ffmpeg_get_mediafile_length, stdout=subprocess.PIPE).stdout.read()
-    real_duration, millis = re.findall(r'(?<=Duration: )(.*?)(?=,)', str(output))[0].split('.')
+def get_recorded_length(file_name, method=2):
+    ffmpeg_get_mediafile_length = ['ffmpeg', '-i', file_name, '-f', 'null', '-']
+    output = subprocess.Popen(ffmpeg_get_mediafile_length,
+                              stdout=subprocess.PIPE,
+                              stderr=subprocess.STDOUT).stdout.read()
+    if method == 1:
+        real_duration, millis = re.findall(r'(?<=Duration: )(.*?)(?=,)', str(output))[0].split('.')
+    else:
+        real_duration, millis = re.findall(r'(?<=time=)(.*?)(?= )', str(output))[0].split('.')
     real_duration = time.strptime(real_duration, '%H:%M:%S')
     real_duration_sec = datetime.timedelta(hours=real_duration.tm_hour,
                                            minutes=real_duration.tm_min,
@@ -50,22 +56,36 @@ def get_recorded_length(file_name):
     return real_duration_sec
 
 
-def cut_recording(file_name, spoitfy_duration):
-    FNULL = open(os.devnull, 'w')
+def cut_recording(file_name, spotify_duration):
+    fnull = open(os.devnull, 'w')
     log.debug('Cutting off start silence')
     copy_name = file_name.replace('.mp3', '.copy.mp3')
-    ffmpeg_silence_remove_cmd = ['ffmpeg', '-i', file_name, '-af', 'silenceremove=start_periods=1:start_threshold=0:detection=peak', '-b:a', '320k',
-     '-ar', '48000', '-f', 'mp3', copy_name]
+    ffmpeg_silence_remove_cmd = ['ffmpeg',
+                                 '-i', file_name,
+                                 '-af', 'silenceremove=start_periods=1:start_threshold=0:detection=peak',
+                                 '-b:a', '320k',
+                                 '-ar', '48000',
+                                 '-f', 'mp3',
+                                 copy_name]
     if not ffmpeg_debug:
-        subprocess.Popen(ffmpeg_silence_remove_cmd, stdout=FNULL, stderr=subprocess.STDOUT).wait()
+        subprocess.Popen(ffmpeg_silence_remove_cmd, stdout=fnull, stderr=subprocess.STDOUT).wait()
     else:
-        output = subprocess.Popen(ffmpeg_silence_remove_cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).stdout.read()
+        output = subprocess.Popen(ffmpeg_silence_remove_cmd,
+                                  stdout=subprocess.PIPE,
+                                  stderr=subprocess.STDOUT).stdout.read()
         log.debug(output)
     os.remove(file_name)
     log.debug('Setting track length')
-    ffmpeg_cut_length_cmd = ['ffmpeg', '-i', copy_name, '-ss', '0', '-t', str(spoitfy_duration), '-b:a', '320k', '-ar', '48000', '-f', 'mp3', file_name]
+    ffmpeg_cut_length_cmd = ['ffmpeg',
+                             '-i', copy_name,
+                             '-ss', '0',
+                             '-t', str(spotify_duration),
+                             '-b:a', '320k',
+                             '-ar', '48000',
+                             '-f', 'mp3',
+                             file_name]
     if not ffmpeg_debug:
-        subprocess.Popen(ffmpeg_cut_length_cmd, stdout=FNULL, stderr=subprocess.STDOUT).wait()
+        subprocess.Popen(ffmpeg_cut_length_cmd, stdout=fnull, stderr=subprocess.STDOUT).wait()
     else:
         output = subprocess.Popen(ffmpeg_cut_length_cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).stdout.read()
         log.debug(output)
@@ -96,5 +116,7 @@ def verify_length(file_name, spotify_duration, play_duration, tolerance=3.0):
 
     if not isclose(real_duration_sec, spotify_duration, abs_tol=tolerance):
         log.error('Recorded {} secs and track is set to {} secs at spotify'.format(real_duration_sec, spotify_duration))
+        log.error('FFMPEG "Duration" reports {} secs and "time" reports {}'.format(
+            get_recorded_length(file_name, 1), get_recorded_length(file_name, 2)))
         return False
     return True
